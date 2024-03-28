712ac420590225a568eab35094ef812f5b64824b
Recep <recepcengiz_96@outlook.com>
2024-03-28 12:28:11 +0300

    sayfa tasarımı yapıldı

--- firebaseExample/src/pages/auth/Login/Login.tsx
+++ firebaseExample/src/pages/auth/Login/Login.tsx
@@ -1,12 +1,35 @@
-import {TextInput, View} from 'react-native';
+import {Button, Text, TextInput, View} from 'react-native';
 import React from 'react';
 import styles from "./Login.style";
+import { Formik } from 'formik';
 
-export default function Login(): React.JSX.Element {
+export default function Login({navigation}): React.JSX.Element {
+
+  const initialState = {
+    username:"",
+    password:""
+  };
+
+  const handleLogin = (formValues) => {
+    console.log(formValues);
+  }
+
   return (
     <View style={styles.container}>
-      <TextInput placeholder='E postanızı giriniz..' style={styles.text} />
-      <TextInput placeholder='Şifrenizi giriniz..' style={styles.text} />
+      <Text style={styles.header}>Bana ne?</Text>
+      <Formik initialValues={initialState} onSubmit={handleLogin}>
+        {
+          ({values,handleChange,handleSubmit}) => (
+            <>
+              <TextInput onChangeText={handleChange("username")} value={values.username} placeholder='E postanızı giriniz..' style={styles.text} />
+              <TextInput onChangeText={handleChange("password")} value={values.password} placeholder='Şifrenizi giriniz..' style={styles.text} />
+              <Button title='Giriş Yap' onPress={handleSubmit} />
+            </>
+            
+            )
+          }
+      </Formik>
+      <Button title='Kayıt Ol' onPress={()=>navigation.navigate("SignUp")} />
     </View>
   );
 }
--- firebaseExample/src/pages/auth/SignUp/SignUp.tsx
+++ firebaseExample/src/pages/auth/SignUp/SignUp.tsx
@@ -1,10 +1,37 @@
-import {Text, View} from 'react-native';
+import {Text, View,TextInput,Button} from 'react-native';
 import React from 'react';
+import styles from "./SignUp.style";
+import { Formik } from 'formik';
 
-export default function SignUp(): React.JSX.Element {
+export default function SignUp({navigation}): React.JSX.Element {
+
+  const initialState = {
+    username:"",
+    password:"",
+    repassword:""
+  };
+
+  const handleFormSubmit = (formValues) => {
+    console.log(formValues);
+  }
+
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View style={styles.container}>
+      <Text>Bana ne?</Text>
+      <Formik initialValues={initialState} onSubmit={handleFormSubmit}>
+        {
+          ({handleSubmit,values,handleChange}) => 
+          (
+            <>
+              <TextInput value={values.username} onChangeText={handleChange("username")} placeholder='E postanızı giriniz..' style={styles.text} />
+              <TextInput value={values.password} onChangeText={handleChange("password")} placeholder='Şifrenizi giriniz..' style={styles.text} />
+              <TextInput value={values.repassword} onChangeText={handleChange("repassword")} placeholder='Şifrenizi giriniz..' style={styles.text} />
+              <Button title='Kayıt Ol' onPress={handleSubmit} />
+            </>
+          )
+        }
+      </Formik>
+      <Button title='Geri' onPress={()=>navigation.goBack()} />
     </View>
   )
 }
